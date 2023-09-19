c4be30cd9bbd678aa850521cc2298e70d9ec6721
João Gabriel Gonçalves <90291555+jgmouradev@users.noreply.github.com>
2023-09-18 22:43:02 -0300

    Add files via upload

---
+++ js/scrollReveal.js
@@ -0,0 +1,7 @@
+
+
+    ScrollReveal().reveal('.foto_apresentacao', { delay: 500 })
+    ScrollReveal().reveal('.texto-apresentacao', { delay: 1000 })
+    ScrollReveal().reveal('.serviços', { delay: 500 })
+    ScrollReveal().reveal('.gallery', { delay: 500 })
+    ScrollReveal().reveal('.time', { delay: 500 })
